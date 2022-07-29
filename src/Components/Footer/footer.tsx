9b6a964dd011046ef8fd21a71e855aeb36a8d973
ENTITY514 <entity514@mail.ru>
2022-07-29 17:04:25 +0600

    beta

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -5,7 +5,7 @@ import style from './footer.module.css'
 export const Footer = () => {
     return (
         <div className={style.container}>
-            <div className={style.text}>Github</div>
+            <a className={style.link} target="_blank"href="https://github.com/ENTITY514/todo-react-app-ts-version-2.0"><div className={style.text}>Github</div></a>
         </div>
     );
 }
